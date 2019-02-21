79ba5e6d202a3f03181532dab95e299772c7bcd3
chenyan <chenyandream@outlook.com>
2019-02-21 09:57:41 +0800

    更改数据库脚本

--- db/sys_schema.sql
+++ db/sys_schema.sql
@@ -1,19 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : localhost_3306
- Source Server Type    : MySQL
- Source Server Version : 50722
- Source Host           : localhost:3306
- Source Schema         : tumo
-
- Target Server Type    : MySQL
- Target Server Version : 50722
- File Encoding         : 65001
-
- Date: 21/12/2018 18:03:35
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
